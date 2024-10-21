5404c62e9aa27e0f97713e5c9bdf17d6f25da57c
Mubaraq Odumeso <mubskill@gmail.com>
2024-10-21 13:27:11 -0500

    Create src.java

---
+++ src.java
@@ -0,0 +1,14 @@
+public class src {
+    public static void main(String[] args)
+    {
+       
+         
+        String name = "tot";
+        String actName;
+
+        if(name == "otot")  System.out.println("My name is  " + name);
+        else System.out.println(" Not correct. IMPOSTER!!!!"); 
+        
+       
+    }
+}
