2fd3bc9f24e2bf9bb2e36260d2e60d277b8a724f
Mubaraq Odumeso <mubskill@gmail.com>
2024-10-21 14:27:47 -0500

    Create feature1.java

---
+++ feature1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class feature1 {
+    public static void main(String[] args)
+    {
+       
+        Scanner ui = new Scanner(System.in);
+        System.out.print("Enter Name: ");
+        String name = ui.nextLine();
+
+        if(name == "max")  
+        {
+            System.out.println("My name is  " + name);
+        }
+        else System.out.println(" Not correct. IMPOSTER!!!!"); 
+        
+
+        ui.close();
+       
+    }
+}
