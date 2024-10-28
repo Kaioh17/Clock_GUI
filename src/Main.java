0b66679942325ff2efbdcb472f6ff2bcc897f1cb
Mubaraq Odumeso <mubskill@gmail.com>
2024-10-28 09:00:37 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,16 @@
-package MAin;
+package src;
+import javax.swing.*;
 
 public class Main {
     public static void main(String[] args)
     {
+        //use SwingUtilities.invokeLater to ensure thred safety 
+        SwingUtilities.invokeLater(() -> {
+            clockApp clock = new clockApp();
+
+            clock.setVisible(true);
+
+        });
        
     }
 }
