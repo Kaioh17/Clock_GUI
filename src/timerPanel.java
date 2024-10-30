c521c96c05a208d1104930165fdca06deb4b4fc1
Mubaraq Odumeso <mubskill@gmail.com>
2024-10-30 12:39:15 -0500

    Update timerPanel.java

--- src/timerPanel.java
+++ src/timerPanel.java
@@ -1,20 +1,33 @@
 package src;
 
-import javax.swing.*;
-import java.awt.*;
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.Font;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+
 
 
 public class timerPanel extends JPanel {
     
+   
+    
+
     
     public timerPanel()
     {
-        setLayout(new BorderLayout());
+
+        setLayout(new FlowLayout());
         JLabel label = new JLabel("Timer functionality goes here!", SwingConstants.CENTER);
         label.setFont(new Font("Arial", Font.PLAIN, 24));
            add(label, BorderLayout.CENTER);
 
            System.out.println("Timer!!");
+
+        
+        
     }
 
 }
