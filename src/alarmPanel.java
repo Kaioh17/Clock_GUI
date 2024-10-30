11c880b53d66c3325ff3fd367f118401f34291d5
Mubaraq Odumeso <mubskill@gmail.com>
2024-10-30 12:39:22 -0500

    Update alarmPanel.java

--- src/alarmPanel.java
+++ src/alarmPanel.java
@@ -7,11 +7,15 @@ public class alarmPanel  extends JPanel{
 
     public alarmPanel()
     {
-        setLayout(new BorderLayout());
-        JLabel label = new JLabel("Alarm functionality goes here!", SwingConstants.CENTER);
-        label.setFont(new Font("Arial", Font.PLAIN, 24));
-           add(label, BorderLayout.CENTER);
+       
 
+           setLayout(new BorderLayout());
+           JLabel label = new JLabel("Alarm functionality goes here!", SwingConstants.CENTER);
+           label.setFont(new Font("Arial", Font.PLAIN, 24));
+              add(label, BorderLayout.CENTER);
+
+              System.out.println("Alarm");
+   
     }
     
 
