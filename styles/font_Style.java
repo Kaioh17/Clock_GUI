1858b9d48cd33c565fc80261f6bb8a3e0645b1b9
Mubaraq Odumeso <mubskill@gmail.com>
2024-10-30 17:43:53 -0500

    Create font_Style.java

---
+++ styles/font_Style.java
@@ -0,0 +1,17 @@
+package styles;
+
+import javax.swing.*;
+
+import src.Main;
+
+import java.awt.*;
+import java.awt.event.*;
+
+public class font_Style extends Main {
+
+    public font_Style()
+    {
+        
+    }
+
+}
