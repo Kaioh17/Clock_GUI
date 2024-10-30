94e9ad60e3a0194e7a56e93909de4437a739025d
Mubaraq Odumeso <mubskill@gmail.com>
2024-10-30 17:43:56 -0500

    Create button_Style.java

---
+++ styles/button_Style.java
@@ -0,0 +1,16 @@
+package styles;
+
+import javax.swing.*;
+
+import src.Main;
+
+import java.awt.*;
+import java.awt.event.*;
+
+public class button_Style extends Main {
+    
+    public button_Style()
+    {
+        
+    }
+}
