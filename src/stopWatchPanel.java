95e2738ddb3f64cf0492631075f39e7bc8d6c201
Mubaraq Odumeso <mubskill@gmail.com>
2024-10-29 22:39:26 -0500

    Update stopWatchPanel.java

--- src/stopWatchPanel.java
+++ src/stopWatchPanel.java
@@ -1,7 +1,21 @@
 package src;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class stopWatchPanel extends JPanel{
+
+    public stopWatchPanel()
+    {
+      
+
+
+        setLayout(new BorderLayout());
+        JLabel label = new JLabel("Stopwatch functionality goes here!", SwingConstants.CENTER);
+        label.setFont(new Font("Arial", Font.PLAIN, 24));
+           add(label, BorderLayout.CENTER);
+
+          
+    }
 
 }
