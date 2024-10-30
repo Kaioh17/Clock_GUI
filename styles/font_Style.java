1b6428b3cc43299a33ae22382357eeaed3695c7a
Mubaraq Odumeso <mubskill@gmail.com>
2024-10-30 18:02:34 -0500

    Update font_Style.java

--- styles/font_Style.java
+++ styles/font_Style.java
@@ -2,16 +2,32 @@ package styles;
 
 import javax.swing.*;
 
-import src.Main;
+//import src.Main;
 
 import java.awt.*;
 import java.awt.event.*;
 
-public class font_Style extends Main {
+public class font_Style  {
 
-    public font_Style()
+    private String fontName;
+    private int style;
+    private int size;
+
+    public static final font_Style ROMAN = new font_Style("Times New Roman",Font.PLAIN,45);
+    public static final font_Style roman = new font_Style("Times New Roman",Font.PLAIN,15);
+
+    
+    public font_Style(String fontName, int style,int size)
     {
-        
+        this.fontName = fontName;
+        this.style = style;
+        this.size = size;
+    }
+
+    //method to access the font object
+    public Font getFont() 
+    {
+        return new Font(fontName, style,size);
     }
 
 }
