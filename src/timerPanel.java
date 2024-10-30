3f8986aa62174e0446771079c5c4e3e59bad7d53
Mubaraq Odumeso <mubskill@gmail.com>
2024-10-29 22:39:13 -0500

    Update timerPanel.java

--- src/timerPanel.java
+++ src/timerPanel.java
@@ -1,7 +1,20 @@
 package src;
 
 import javax.swing.*;
+import java.awt.*;
+
 
 public class timerPanel extends JPanel {
+    
+    public timerPanel()
+    {
+        setLayout(new BorderLayout());
+        JLabel label = new JLabel("Stopwatch functionality goes here!", SwingConstants.CENTER);
+        label.setFont(new Font("Arial", Font.PLAIN, 24));
+           add(label, BorderLayout.CENTER);
+
+    }
+    
 
 }
+
