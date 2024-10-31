1902fd8ce89100aca27df087f60e15c70d37708d
Mubaraq Odumeso <mubskill@gmail.com>
2024-10-31 10:32:06 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 package src;
 
 import java.awt.*;
+import styles.*;
 import java.awt.event.*;
 import javax.swing.*;
 import javax.swing.border.Border;
@@ -8,42 +9,50 @@ import javax.swing.border.Border;
 public class Main extends JFrame 
 {
     private CardLayout cardLayout;
-    private JPanel clockPanel;
+    private JPanel mainPanel;
 
   
 public Main()
     {
         //Set up for main frame
         setTitle("Clock");
-        setSize(500,300);
+        setSize(550,330);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setResizable(true);
        
 
         //cardLayout for switching betwen panels
         cardLayout = new CardLayout();
-        clockPanel = new JPanel(cardLayout);
+        mainPanel = new JPanel(cardLayout);
 
         // add the classes to the main panel
-        clockPanel.add(new timerPanel(), "Timer");
-        clockPanel.add(new alarmPanel(), "Alarm");
-        clockPanel.add(new stopWatchPanel(), "Stop Watch");
+        mainPanel.add(new timerPanel(), "Timer");
+        mainPanel.add(new alarmPanel(), "Alarm");
+        mainPanel.add(new stopWatchPanel(), "Stop Watch");
 
         //create buttons
         JButton timerButton = new JButton("Timer");
         JButton alarmButton = new JButton("Alarm");
         JButton stopWatchButton = new JButton("Stop Watch");
 
+        //edit the button
+        timerButton.setBackground(Color.GREEN);
+        timerButton.setForeground(Color.BLACK);
+        timerButton.setFont(font_Style.roman.getFont());
+       
+
         //Action listeners for the button functions(using lambda)
-        timerButton.addActionListener(e -> cardLayout.show(clockPanel, "Timer"));
-        alarmButton.addActionListener(e -> cardLayout.show(clockPanel, "Alarm"));
-        stopWatchButton.addActionListener(e -> cardLayout.show(clockPanel, "Stop Watch"));
+        timerButton.addActionListener(e -> cardLayout.show(mainPanel, "Timer"));
+        alarmButton.addActionListener(e -> cardLayout.show(mainPanel, "Alarm"));
+        stopWatchButton.addActionListener(e -> cardLayout.show(mainPanel, "Stop Watch"));
 
         //A panel for the buttons
         JPanel buttonPanel = new JPanel();
         buttonPanel.add(timerButton);
         buttonPanel.add(alarmButton);
         buttonPanel.add(stopWatchButton);
+       
+        
 
         /**
         Panel to set clock and calender in real time *          
@@ -52,8 +61,8 @@ public Main()
        clock_Panel.add(new clockPanel());
 
         //Add panels to the main frame
-        add(buttonPanel, BorderLayout.SOUTH);
-        add(clockPanel, BorderLayout.CENTER);
+        add(buttonPanel,BorderLayout.SOUTH);
+        add(mainPanel, BorderLayout.CENTER);
         add(clock_Panel,BorderLayout.NORTH);
         
         
