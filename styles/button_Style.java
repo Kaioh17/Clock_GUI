58770dd583f6a7bd9cb443c8c367b765724c6b23
Mubaraq Odumeso <mubskill@gmail.com>
2024-11-02 23:00:10 -0500

    Update button_Style.java

--- styles/button_Style.java
+++ styles/button_Style.java
@@ -2,15 +2,30 @@ package styles;
 
 import javax.swing.*;
 
-import src.Main;
 
 import java.awt.*;
-import java.awt.event.*;
 
-public class button_Style extends Main {
+
+public class button_Style {
     
-    public button_Style()
+    //a method to hold the button ui edit layout using (varags) to handle as many buttons as possible
+    public static void mainStyle(JButton... buttons)
     {
-        
+        for(JButton button : buttons )
+        {
+            button.setBackground(Color.BLACK);
+            button.setFont(font_Style.roman.getFont());
+        }
     }
+
+    public static void attributeStyle(JButton... buttons)
+    {
+        for(JButton button : buttons)
+        {
+            button.setBackground(Color.BLACK);
+            button.setFont(font_Style.roman.getFont());
+        }
+    }
+
+    
 }
