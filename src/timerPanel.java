cd0f2c4761ab83dd128b383530c17933da7110ee
Mubaraq Odumeso <mubskill@gmail.com>
2024-11-06 13:15:19 -0600

    Update timerPanel.java

--- src/timerPanel.java
+++ src/timerPanel.java
@@ -1,33 +1,60 @@
 package src;
 
-import java.awt.BorderLayout;
-import java.awt.FlowLayout;
-import java.awt.Font;
-
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.SwingConstants;
-
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import styles.*;
 
 
 public class timerPanel extends JPanel {
     
-   
-    
+   private JLabel timerLabel;
+   private Timer timer;
+   private setTimerFrame setFrame;
+   private JButton setButton;
+   private JButton pauseButton;
+   private JPanel buttonPanel;
 
     
     public timerPanel()
     {
 
-        setLayout(new FlowLayout());
-        JLabel label = new JLabel("Timer functionality goes here!", SwingConstants.CENTER);
-        label.setFont(new Font("Arial", Font.PLAIN, 24));
-           add(label, BorderLayout.CENTER);
+         //timer label   (00:00:00)
+         timerLabel = new JLabel("00:00.00");
+         timerLabel.setFont(font_Style.ROMAN.getFont());
+         timerLabel.setHorizontalAlignment(SwingConstants.CENTER);
+         timerLabel.setForeground(Color.BLACK);
+//         timerLabel.setBounds(10,10,300,100);
 
-           System.out.println("Timer!!");
+         //initialize component
+         buttonPanel = new JPanel();
+         setButton = new JButton("Set");//set will prompt a new frame to set timer
+         pauseButton = new JButton("pause");
 
-        
-        
+         //edit panel
+//        setLayout(null);
+//         buttonPanel.setBounds(50, 120, 500, 50);
+         buttonPanel.setBackground(Color.pink);
+
+        //set button functions
+        setButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+               setFrame = new setTimerFrame();
+               setFrame.setVisible(true);
+                System.out.println("set timer");
+            }
+        });
+         //edit button
+        button_Style.attributeStyle(setButton);
+        button_Style.stopStyle(pauseButton);
+
+        //add buttons to the panel
+         buttonPanel.add(setButton);
+         buttonPanel.add(pauseButton);
+
+         add(timerLabel, BorderLayout.CENTER);
+         add(buttonPanel, BorderLayout.SOUTH);
     }
 
 }
