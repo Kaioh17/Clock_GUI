a214ae583b45f2178bd2ced85d3b248ce7b83fa5
Mubaraq Odumeso <mubskill@gmail.com>
2024-11-08 14:45:34 -0600

    Update button_Style.java

--- styles/button_Style.java
+++ styles/button_Style.java
@@ -1,6 +1,7 @@
 package styles;
 
 import javax.swing.*;
+import javax.swing.border.Border;
 
 
 import java.awt.*;
@@ -16,6 +17,7 @@ public class button_Style {
             button.setBackground(Color.BLACK);
             button.setForeground(Color.WHITE);
             button.setFont(font_Style.roman.getFont());
+            button.setFocusable(false);
             // button.setBorder(null);
             button.setSize(150, 10);
         }
@@ -25,9 +27,16 @@ public class button_Style {
     {
         for(JButton button : buttons)
         {
-            button.setBackground(Color.BLUE);
+        button.setBackground(new Color(183, 85, 0));
             button.setForeground(Color.WHITE);
-            button.setFont(font_Style.roman.getFont());
+            button.setFont(font_Style.sansSerif.getFont());
+            button.setFocusable(false);
+            button.setSize(150, 10);
+            button.setBorderPainted(false);
+            button.setContentAreaFilled(false); // Remove the default button area fill
+            button.setOpaque(true);
+
+
         }
     }
 
@@ -37,20 +46,26 @@ public class button_Style {
         {
             button.setBackground(Color.RED); 
             button.setForeground(Color.WHITE);
-            button.setFont(font_Style.roman.getFont());
+            button.setFont(font_Style.sansSerif.getFont());
+            button.setFocusable(false);
+            button.setSize(150, 10);
+            button.setBorderPainted(false);
+            button.setContentAreaFilled(false); // Remove the default button area fill
+            button.setOpaque(true);
+
         }
     }
     public static void up_downStyle(JButton... buttons)
     {
         for(JButton button : buttons)
         {
-            button.setBackground(Color.RED);
-            button.setForeground(Color.WHITE);
+            button.setBackground(Color.CYAN);
+            button.setForeground(Color.BLACK);
             button.setFont(font_Style.roman.getFont());
+            button.setFocusable(false);
+            button.setSize(150, 10);
         }
     }
-
-
 
 
 
