108de6d79dc3a1bfb8fa2e97539c377e99fb1e0d
Mubaraq Odumeso <mubskill@gmail.com>
2024-11-08 10:51:26 -0600

    Update setTimerFrame.java

--- src/setTimerFrame.java
+++ src/setTimerFrame.java
@@ -3,51 +3,112 @@ package src;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import styles.*;
 
 public class setTimerFrame extends JFrame {
 
     private JButton startButton;
     private JPanel buttonPanel;
     private JPanel valuePanel;
-
-    private JPanel hourPanel;
-    private JPanel minutePanel;
-    private JPanel secondPanel;
-
-   private JButton upButton;
-   private JButton downButton;
+    private JButton upButton;
+    private JButton downButton;
+    private JTextField numberField;
+    private int hour;
+    private int minute;
+    private int second;
 
     public setTimerFrame() {
-        //an ui for users to set timer
+        //create an ui for users to set timer
         setTitle("Set Timer");
-        setSize(400,200);
+        setSize(400, 200);
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         setResizable(false);
         setBackground(Color.BLACK);
+        setLayout(new FlowLayout());
 
-        //create a
+        //create a panel to contain the
         valuePanel = new JPanel();
+        valuePanel.setBackground(Color.BLACK);
+        valuePanel.setLayout(new GridLayout(1,3, 10,5));
 
-        //hour panel
-        hourPanel = new JPanel();
-        hourPanel.setBackground(Color.BLACK);
+        valuePanel.setSize(400, 200);
 
-        //minute Panel
-        minutePanel = new JPanel();
-        minutePanel.setBackground(Color.BLACK);
-
-        // second panel
-        secondPanel = new JPanel();
-        secondPanel.setBackground(Color.BLACK);
+       setHourValue();
+       setMinuteValue();
 
 
-        //add panel
+
+        add(valuePanel, BorderLayout.SOUTH);
+
+    }
+
+    private void setHourValue()
+    {
+        JButton upButton = new JButton(">");
+        JTextField numberField = new JTextField(String.valueOf(hour), 5 );
+        JButton downButton = new JButton("<");
+
+    //Edit buttons
+        button_Style.attributeStyle(upButton,downButton);
+
+
+        //Assigning functions to the up & down buttons
+        upButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                try{
+                    if(hour <= 24) hour++;// to increment as long as hour not greater than 24
+                    else hour=0;
+                    numberField.setText(String.valueOf(hour));
+                }
+                catch(Exception ex){
+                    JOptionPane.showMessageDialog(null, "Error updating hour" +ex.getMessage(),"error", JOptionPane.ERROR_MESSAGE);
+                }
+
+            }
+        });
+
+        downButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                try
+                {
+                    if(hour > 0)hour-- ;
+                    else hour = 0;
+                    numberField.setText(String.valueOf(hour));
+                }
+                catch(Exception ex){
+                    JOptionPane.showMessageDialog(null, "Error updating hour" +ex.getMessage(),"error", JOptionPane.ERROR_MESSAGE);
+                }
+            }
+        });
+
+
+        JPanel hourPanel = new JPanel();
+        hourPanel.setBackground(Color.RED);
+        hourPanel.setLayout(new GridLayout(3,3));
+
+        hourPanel.add(upButton);
+        hourPanel.add(numberField);
+        hourPanel.add(downButton);
+
         valuePanel.add(hourPanel);
+    }
+
+    private void setMinuteValue()
+    {
+        JButton upButton = new JButton(">");
+        JTextField numberField = new JTextField(String.valueOf(hour), 5 );
+        JButton downButton = new JButton("<");
+
+
+        JPanel minutePanel = new JPanel();
+        minutePanel.setBackground(Color.RED);
+        minutePanel.setLayout(new GridLayout(3,3));
+
+        minutePanel.add(upButton);
+        minutePanel.add(numberField);
+        minutePanel.add(downButton);
+
         valuePanel.add(minutePanel);
-        valuePanel.add(secondPanel);
-
-        add(valuePanel);
-
     }
 
 }
