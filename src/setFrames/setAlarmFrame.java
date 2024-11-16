27bb8aa9b888796d46daf10765ebc4b4042d307d
Mubaraq Odumeso <mubskill@gmail.com>
2024-11-16 13:47:42 -0600

    Update setAlarmFrame.java

--- src/setFrames/setAlarmFrame.java
+++ src/setFrames/setAlarmFrame.java
@@ -34,7 +34,7 @@ public class setAlarmFrame extends JFrame {
         setTimePanel.add(downIcon());
 
 
-        File file = new File("Clock_Gui/src/Sounds/timer22.png");
+        File file = new File("Clock_Gui/src/Sounds/upIcon.png");
 
         System.out.println("File exists: " + file.exists());
         System.out.println("Absolute path: " + file.getAbsolutePath());
