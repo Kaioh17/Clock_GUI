cb8c3ade4cc6def9fc61e29d57ec09f127f9bfe2
Mubaraq Odumeso <mubskill@gmail.com>
2024-11-16 13:46:26 -0600

    Update font_Style.java

--- styles/font_Style.java
+++ styles/font_Style.java
@@ -1,7 +1,5 @@
 package styles;
 
-import javax.swing.*;
-
 //import src.Main;
 
 import java.awt.*;
@@ -15,7 +13,7 @@ public class font_Style  {
 
 
     //initialized font style method for easier editing and control
-    public static final font_Style ROMAN = new font_Style("Times New Roman",Font.PLAIN,55);
+    public static final font_Style serif = new font_Style("SansSerif",Font.PLAIN,65);
     public static final font_Style roman = new font_Style("Times New Roman",Font.PLAIN,18);
     public static final font_Style ROMANII = new font_Style("Malgun Gothic",Font.PLAIN,35);
     public static final font_Style sansSerif = new font_Style("SansSerif",Font.BOLD,20);
