1f6f63e8d1abcd8e1d19adff64d6b26f428b5023
Mubaraq Odumeso <mubskill@gmail.com>
2024-11-16 13:45:32 -0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ import java.awt.*;
 import src.panels.*;
 import styles.*;
 import java.awt.event.*;
+import java.io.File;
 import javax.swing.*;
 
 public class Main extends JFrame 
@@ -15,12 +16,10 @@ public class Main extends JFrame
 public Main()
     { //Set up for main frame
         setTitle("Clock");
-        setSize(550,330);
+        setSize(510,450);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setResizable(true);
         getContentPane().setBackground(Color.BLACK);
-
-
 
         //cardLayout for switching between panels
         cardLayout = new CardLayout();
@@ -34,21 +33,46 @@ public Main()
         mainPanel.add(new alarmPanel(), "Alarm");
         mainPanel.add(new stopWatchPanel(), "Stop Watch");
 
-//        //Icon test
-//        ImageIcon timerIcon = new ImageIcon("src/Sounds/timer22.png");
-//        // Resize icon if necessary
-//        Image scaledImage = timerIcon.getImage().getScaledInstance(50, 50, Image.SCALE_SMOOTH);
-//        ImageIcon scaledIcon = new ImageIcon(scaledImage);
+        //Add icon to buttons(timer)
+        ImageIcon timerIcon = new ImageIcon("Clock_Gui/src/Sounds/timerIconW.png");
+        // Resize icon if necessary
+        Image scaledImage = timerIcon.getImage().getScaledInstance(25, 25, Image.SCALE_SMOOTH);
+        ImageIcon scaledIcon = new ImageIcon(scaledImage);
+
+        //Add icon to buttons(stopwatch)
+        ImageIcon stopWatchIcon = new ImageIcon("Clock_Gui/src/Sounds/stopWatchIconW.png");
+        // Resize icon if necessary
+        Image scaledImage2 = stopWatchIcon.getImage().getScaledInstance(30, 30, Image.SCALE_SMOOTH);
+        ImageIcon stopwatchScaledIcon = new ImageIcon(scaledImage2);
+
+        //Add icon to buttons(alarm)
+        ImageIcon alarmIcon = new ImageIcon("Clock_Gui/src/Sounds/alarmIconW.png");
+        // Resize icon if necessary
+        Image alarmScaledImage = alarmIcon.getImage().getScaledInstance(25, 25, Image.SCALE_SMOOTH);
+        ImageIcon alarmScaledIcon = new ImageIcon(alarmScaledImage);
+
+        //Check if file exists
+        File file = new File("Clock_Gui/src/Sounds/stopwatch.png");
+
+        System.out.println("File exists: " + file.exists());
+        System.out.println("Absolute path: " + file.getAbsolutePath());
 
         //create buttons
-        JButton timerButton = new JButton("Timer");
-        JButton alarmButton = new JButton("Alarm");
-        JButton stopWatchButton = new JButton("Stop Watch");
+        JButton timerButton = new JButton();
+        JButton alarmButton = new JButton();
+        JButton stopWatchButton = new JButton();
+
+        //add the icons to buttons
+        timerButton.setIcon(scaledIcon);
+        stopWatchButton.setIcon(stopwatchScaledIcon);
+        alarmButton.setIcon(alarmScaledIcon);
 
 
 
         //edit the button
         button_Style.mainStyle(timerButton, alarmButton,  stopWatchButton);
+//        timerButton.setPreferredSize(new Dimension(150,58));
+//        stopWatchButton.setPreferredSize(new Dimension(150,58));
        
        
 
@@ -67,9 +91,30 @@ public Main()
 
             @Override
             public void actionPerformed(ActionEvent e) {
-                timerButton.setForeground(new Color(183, 85, 0));
-                alarmButton.setForeground(Color.lightGray);
-                stopWatchButton.setForeground(Color.lightGray);
+
+
+                //Change icon image
+                ImageIcon timerIcon = new ImageIcon("Clock_Gui/src/Sounds/timerIconOrange.png");
+              // Resize icon if necessary
+                Image scaledImage = timerIcon.getImage().getScaledInstance(25, 25, Image.SCALE_SMOOTH);
+                ImageIcon scaledIcon = new ImageIcon(scaledImage);
+
+                timerButton.setIcon(scaledIcon);
+
+                //Add icon to buttons
+                ImageIcon stopWatchIcon = new ImageIcon("Clock_Gui/src/Sounds/stopWatchIconW.png");
+                // Resize icon if necessary
+                Image scaledImage2 = stopWatchIcon.getImage().getScaledInstance(30, 30, Image.SCALE_SMOOTH);
+                ImageIcon stopwatchScaledIcon = new ImageIcon(scaledImage2);
+
+                stopWatchButton.setIcon(stopwatchScaledIcon);
+
+                ImageIcon alarmIcon = new ImageIcon("Clock_Gui/src/Sounds/alarmIconW.png");
+                // Resize icon if necessary
+                Image alarmScaledImage = alarmIcon.getImage().getScaledInstance(25, 25, Image.SCALE_SMOOTH);
+                ImageIcon alarmScaledIcon = new ImageIcon(alarmScaledImage);
+
+                alarmButton.setIcon(alarmScaledIcon);
             }
         });
 
@@ -78,9 +123,29 @@ public Main()
 
             @Override
             public void actionPerformed(ActionEvent e) {
-                alarmButton.setForeground(new Color(183, 85, 0));
-                timerButton.setForeground(Color.lightGray);
-                stopWatchButton.setForeground(Color.lightGray);
+
+                //Change icon image
+                ImageIcon timerIcon = new ImageIcon("Clock_Gui/src/Sounds/timerIconW.png");
+                // Resize icon if necessary
+                Image scaledImage = timerIcon.getImage().getScaledInstance(25, 25, Image.SCALE_SMOOTH);
+                ImageIcon scaledIcon = new ImageIcon(scaledImage);
+
+                timerButton.setIcon(scaledIcon);
+
+                //Add icon to buttons
+                ImageIcon stopWatchIcon = new ImageIcon("Clock_Gui/src/Sounds/stopWatchIconW.png");
+                // Resize icon if necessary
+                Image scaledImage2 = stopWatchIcon.getImage().getScaledInstance(30, 30, Image.SCALE_SMOOTH);
+                ImageIcon stopwatchScaledIcon = new ImageIcon(scaledImage2);
+
+                stopWatchButton.setIcon(stopwatchScaledIcon);
+
+                ImageIcon alarmIcon = new ImageIcon("Clock_Gui/src/Sounds/alarmIconOrange.png");
+                // Resize icon if necessary
+                Image alarmScaledImage = alarmIcon.getImage().getScaledInstance(25, 25, Image.SCALE_SMOOTH);
+                ImageIcon alarmScaledIcon = new ImageIcon(alarmScaledImage);
+
+                alarmButton.setIcon(alarmScaledIcon);
             }
         });
 
@@ -88,11 +153,29 @@ public Main()
 
             @Override
             public void actionPerformed(ActionEvent e) {
-                alarmButton.setForeground(Color.lightGray);
-                timerButton.setForeground(Color.lightGray);
-                stopWatchButton.setForeground(new Color(183, 85, 0));
 
+                //Change icon image
+                ImageIcon timerIcon = new ImageIcon("Clock_Gui/src/Sounds/timerIconW.png");
+                // Resize icon if necessary
+                Image scaledImage = timerIcon.getImage().getScaledInstance(25, 25, Image.SCALE_SMOOTH);
+                ImageIcon scaledIcon = new ImageIcon(scaledImage);
 
+                timerButton.setIcon(scaledIcon);
+
+                //Add icon to buttons
+                ImageIcon stopWatchIcon = new ImageIcon("Clock_Gui/src/Sounds/stopWatchOrange.png");
+                // Resize icon if necessary
+                Image scaledImage2 = stopWatchIcon.getImage().getScaledInstance(30, 30, Image.SCALE_SMOOTH);
+                ImageIcon stopwatchScaledIcon = new ImageIcon(scaledImage2);
+
+                stopWatchButton.setIcon(stopwatchScaledIcon);
+
+                ImageIcon alarmIcon = new ImageIcon("Clock_Gui/src/Sounds/alarmIconW.png");
+                // Resize icon if necessary
+                Image alarmScaledImage = alarmIcon.getImage().getScaledInstance(25, 25, Image.SCALE_SMOOTH);
+                ImageIcon alarmScaledIcon = new ImageIcon(alarmScaledImage);
+
+                alarmButton.setIcon(alarmScaledIcon);
             }
         });
         //A panel for the buttons
@@ -102,15 +185,16 @@ public Main()
         buttonPanel.add(stopWatchButton);
 
         buttonPanel.setBackground(Color.BLACK);
+        buttonPanel.setLayout(new FlowLayout());
+
        
         
 
         /*
-        Panel to set clock and calender in real time *          
+        Panel to set clock and calendar in real time *
         */
         JPanel clock_Panel = new JPanel();
        clock_Panel.add(new clockPanel());
-
        clock_Panel.setBackground(Color.BLACK);
 
         //Add panels to the main frame
