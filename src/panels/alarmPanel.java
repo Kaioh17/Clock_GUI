f09be87ba1b0a91c817a9b24dd48ffbfe7a9566a
Mubaraq Odumeso <mubskill@gmail.com>
2024-11-24 18:02:06 -0600

    Update alarmPanel.java

--- src/panels/alarmPanel.java
+++ src/panels/alarmPanel.java
@@ -3,6 +3,7 @@ package src.panels;
 import src.setFrames.setAlarmFrame;
 import styles.*;
 
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -14,6 +15,7 @@ public class alarmPanel  extends JPanel{
     public alarmPanel()
     {
        setBackground(Color.BLACK);
+       setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 
        //Add image icon
        ImageIcon addIcon = new ImageIcon("Clock_Gui/src/Import/add.png");
@@ -22,6 +24,7 @@ public class alarmPanel  extends JPanel{
 
         //button to set alarm
        setAlarmButton = new JButton();
+
        setAlarmButton.setIcon(scaledIcon);
 
 
@@ -35,13 +38,27 @@ public class alarmPanel  extends JPanel{
         setAlarmButton.addActionListener(e -> openSetAlarmFrame() );
        //add to main panel
         add(setAlarmButton);
+
+        // Initialize the panel where alarms will be displayed
+        alarmSlot = new JPanel();
+        alarmSlot.setLayout(new BoxLayout(alarmSlot, BoxLayout.Y_AXIS));
+        alarmSlot.setBackground(Color.BLACK);
+        add(alarmSlot);
     }
 
     //A method to open the set timer frame
     private void openSetAlarmFrame() {
-        setAlarmFrame setFrame = new setAlarmFrame();
+        setAlarmFrame setFrame = new setAlarmFrame(this);
         setFrame.setVisible(true);
     }
 
-
+    // Method to add alarms to the display panel
+    public void addAlarm(String alarmText) {
+        JLabel alarmLabel = new JLabel(alarmText);
+        alarmLabel.setFont(font_Style.sansSerif.getFont());
+        alarmLabel.setForeground(Color.WHITE);
+        alarmSlot.add(alarmLabel);
+        alarmSlot.revalidate();
+        alarmSlot.repaint();
+    }
 }
